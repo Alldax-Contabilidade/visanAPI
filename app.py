08f2af53a895976e553467544f556974c0acd4d3
unknown <matheus.barbosa.oliveira00@gmail.com>
2024-04-12 10:21:50 -0300

    atualizando repositorio com modificacoes de locais de receber os arquivos

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ from werkzeug.utils import secure_filename
 from flask import send_from_directory, jsonify
 
 app = Flask(__name__)
-UPLOAD_FOLDER = './data'
-
+# UPLOAD_FOLDER = './data'
+UPLOAD_FOLDER = r'T:\CLIENTES\GRUPO VISAN\INTEGRACAO\ARQUIVOS BENNER'
+UPLOAD_FOLDER_ALTER = r'C:\Users\VM100\Documents\RepositorioGit\visanAPI\data'
 
 ALLOWED_EXTENSIONS = {'zip'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config['UPLOAD_FOLDER_ALTER'] = UPLOAD_FOLDER_ALTER
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/visan', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         
@@ -36,8 +38,13 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            try:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return "arquivo salvo com sucesso"
+            except:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER_ALTER'], filename))
+                return "arquivo salvo na pasta data"
+            # return redirect(url_for('download_file', name=filename))
     return '''
     <!doctype html>
     <html>
@@ -59,5 +66,7 @@ def upload_file():
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+
 if __name__ == '__main__':
-    app.run(host="localhost", port=8501, debug = True)
+    app.run(host="192.168.75.114", port=80, debug = True)
+    # host="192.168.75.114"
